ae83f9adc2354802fc54cd63fb1006a523da831a
AnaKlaussen <ana.luiza.klaussen@gmail.com>
2026-05-13 18:55:53 -0300

    Transforma aplicação em RESTful API
    utilizando exemplos de livros conhecidos.

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+livros = [
+    {"id": 1, "titulo": "Corte de Espinhos e Rosas"},
+    {"id": 2, "titulo": "Quarta Asa"}
+]
+
+@app.route('/', methods=['GET'])
+def listar_livros():
+    return jsonify(livros)
+
+@app.route('/', methods=['POST'])
+def adicionar_livro():
+    novo_livro = request.json
+    livros.append(novo_livro)
+
+    return jsonify({
+        "mensagem": "Livro adicionado com sucesso!",
+        "livro": novo_livro
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
